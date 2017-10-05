fbaa93aeb8aa689f9c0cf00bef64334cdbe650a6
Eric Thoreson <ethoreso@uncc.edu>
2017-10-05 23:15:16 +0000

    Done Section 4.5

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,10 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.html.haml by default
   end
+  def new
+    @movie = Movie.new
+    # default: render 'new' template
+  end
   private def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
