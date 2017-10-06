b9ac60ef09e622c5367cb1233cb0ac5072324e61
Eric Thoreson <ethoreso@uncc.edu>
2017-10-06 00:22:20 +0000

    Done Section 4.10

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,7 +23,7 @@ class MoviesController < ApplicationController
   end
   def update
     @movie = Movie.find params[:id]
-    @movie.update_attributes!(params[:movie])  # old way
+    #@movie.update_attributes!(params[:movie])  # old way
     @movie.update_attributes!(movie_params)  # new way  
     flash[:notice] = "#{@movie.title} was successfully updated."
     redirect_to movie_path(@movie)
